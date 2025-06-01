7fc165317e395ee280f1b598d366fa686c3f48e3
ena-isme <dev.shinena@gmail.com>
2025-06-02 00:26:38 +0900

    edit to force cache

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'front',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'front',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
