cbc9f512d0fc8efeff3c8b02c9970803793581c4
ena-isme <dev.shinena@gmail.com>
2025-06-02 00:20:38 +0900

    force cache delete

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDdGYWdfSY8E0Hiee19RNK2C5iIBhoMa2M",
+  authDomain: "seewonschool-f47ca.firebaseapp.com",
+  projectId: "seewonschool-f47ca",
+  storageBucket: "seewonschool-f47ca.firebasestorage.app",
+  messagingSenderId: "587335011034",
+  appId: "1:587335011034:web:a6fcd3aa479daa87b7b3e3",
+  measurementId: "G-7RC0TW37XJ"
+};
